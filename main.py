83534313563091382d0ef699c40f71016423c6c8
Jayden-2008 <jbethune@brooklynsteamcenter.org>
2025-12-11 10:53:31 -0500

    .

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ def index():
 
 @app.route("/browse")
 def browse():
+   
    connection = conncet_db()
 
 
@@ -53,3 +54,20 @@ def browse():
 
    return render_template("browse.html.jinja", products=result)
 
+@app.route("/product/<product_id>")
+def product_page(product_id):
+    connection = conncet_db()
+
+
+    cursor = connection.cursor()
+
+
+    cursor.execute("SELECT * FROM `Product` WHERE `ID` = %s", (product_id))
+
+
+    result = cursor.fetchone()
+
+
+    connection.close()
+
+    return render_template("product.html.jinja", product=result)
